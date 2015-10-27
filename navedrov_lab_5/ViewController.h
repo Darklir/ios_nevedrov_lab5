c450216d82fc8033b3a9a97c977de23d3f33b4d0
Admin <admin@mac-admin.local>
2015-10-27 19:57:33 +0200

    Initial Commit

---
+++ navedrov_lab_5/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  navedrov_lab_5
+//
+//  Created by Admin on 27.10.15.
+//  Copyright © 2015 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ navedrov_lab_5/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  navedrov_lab_5
+//
+//  Created by Admin on 27.10.15.
+//  Copyright © 2015 Admin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
